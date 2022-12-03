fac94baca787f990ac4ab6151be4d9061d0a107d
ac-reynolds <adam.chiu.reynolds@gmail.com>
2022-12-02 18:38:55 -0800

    creates skeleton for ui

---
+++ src/MyComponent.tsx
@@ -0,0 +1,11 @@
+
+function MyComponent() {
+	return (
+	<>
+		<button>Click me to complete the phrase below.</button>
+		<p id="phrase">"I really need to..."</p>
+	</>
+	);
+}
+
+export default MyComponent;
