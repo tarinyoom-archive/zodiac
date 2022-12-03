89c799451bf10e5de9442b9ccac997e166d4c5c8
ac-reynolds <adam.chiu.reynolds@gmail.com>
2022-12-03 11:54:11 -0800

    adds priming support for each sign

--- src/Controller.ts
+++ src/Controller.ts
@@ -1,19 +1,54 @@
-import getCompletion from "./BackendSurface";
+import { getCompletion } from "./BackendSurface";
+import examples from "./examples.json";
+
+async function buildHoroscope(sign: string): Promise<string> {
+
+	console.log("building for " + sign);
+	const prefix = (examples as any)[sign].join("\n--\n") + "\n--\n";
+	const completion = await getCompletion(prefix);
+
+	// clean horoscope
+	const fragments = completion.split("\n");
+	const longestFragment = fragments.reduce((prev: string, curr: string) => curr.length < prev.length ? prev : curr, "");
+	let start, end;
+	for (start = 0; start < longestFragment.length; start++) {
+		const ascii = longestFragment.charCodeAt(start);
+		if (65 <= ascii && ascii < 91) { // if ascii char is upper case
+			break;
+		}
+	}
+
+	for (end = longestFragment.length - 1; end > start; end--) {
+		const ascii = longestFragment.charCodeAt(end);
+		if (ascii === 46) {
+			break;
+		}
+	}
+
+	const trimmed = longestFragment.substring(start, end + 1);
+
+	return trimmed;
+}
+
+export function isValidSign(sign: string) {
+	return sign in examples;
+}
 
 export async function setHoroscope(
 		sign: string,
-		signDescriptionTextId: string,
+		signTextId: string,
 		horoscopeTextId: string) {
 
-	const signDescription = document.getElementById(signDescriptionTextId);
+	const signText = document.getElementById(signTextId);
 	const horoscope = document.getElementById(horoscopeTextId);
 
-	if (signDescription) {
-		signDescription.textContent = `${sign}. <description here>`;
-	}
-
 	if (horoscope) {
-		horoscope.textContent = await getCompletion("You are fierce, loyal, and caring. Without going into specifics, your day will vaguely be as follows.");
+		horoscope.textContent = await buildHoroscope(sign).then((horoscope: string) => {
+			if (signText) {
+				signText.textContent = `${sign}. <TODAY'S DATE here>`;
+			}
+			return horoscope;
+		});
 	}
 }
 
