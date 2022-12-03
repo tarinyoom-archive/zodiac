0a083a1867d1c576335ce7590b6fbd2ffa87f7df
tuladhara <tuladhar.aditya@gmail.com>
2022-12-02 20:18:37 -0800

    Adding button phrase completion and installing @mui/material

--- src/MyComponent.tsx
+++ src/MyComponent.tsx
@@ -1,11 +1,24 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import { useState } from "react";
 
 function MyComponent() {
-	return (
-	<>
-		<button>Click me to complete the phrase below.</button>
-		<p id="phrase">"I really need to..."</p>
-	</>
-	);
+  const [text, setText] = useState(" ");
+
+  return (
+    <>
+      <Button
+        variant="contained"
+        onClick={() => {
+          setText("eat some food");
+        }}
+      >
+        Click me to complete the phrase below.
+      </Button>
+      <p id="phrase">"I really need to... {text}"</p>
+      <p> </p>
+    </>
+  );
 }
 
 export default MyComponent;
