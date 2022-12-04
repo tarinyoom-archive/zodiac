56c34a1c3b823c3cb2991f1addab3bb7048d79cd
tuladhara <tuladhar.aditya@gmail.com>
2022-12-03 20:19:36 -0800

    Debugging date input issues

--- src/Controller.ts
+++ src/Controller.ts
@@ -1,3 +1,4 @@
+import { stringify } from "querystring";
 import { getCompletion } from "./BackendSurface";
 import examples from "./examples.json";
 
@@ -30,6 +31,106 @@ async function buildHoroscope(sign: string): Promise<string> {
 	return trimmed;
 }
 
+//export function findZodiacSign(userBirthMonth: number, userBirthDay: number):string
+export function findZodiacSign(userBirthDate: Date):string
+{
+	var astro_sign:string;
+
+	//const month:number = userBirthMonth;
+	//const day:number = userBirthDay;
+	
+	const month:number = userBirthDate.getMonth();
+	const day:number = userBirthDate.getDay();
+
+	if (month == 12){
+               
+            if (day < 22)
+            astro_sign = "Sagittarius";
+            else
+            astro_sign ="Capricorn";
+        }
+               
+        else if (month == 1){
+            if (day < 20)
+            astro_sign = "Capricorn";
+            else
+            astro_sign = "Aquarius";
+        }
+               
+        else if (month == 2){
+            if (day < 19)
+            astro_sign = "Aquarius";
+            else
+            astro_sign = "Pisces";
+        }
+               
+        else if(month == 3){
+            if (day < 21)
+            astro_sign = "Pisces";
+            else
+            astro_sign = "Aries";
+        }
+        else if (month == 4){
+            if (day < 20)
+            astro_sign = "Aries";
+            else
+            astro_sign = "Taurus";
+        }
+               
+        else if (month == 5){
+            if (day < 21)
+            astro_sign = "Taurus";
+            else
+            astro_sign = "Gemini";
+        }
+               
+        else if( month == 6){
+            if (day < 21)
+            astro_sign = "Gemini";
+            else
+            astro_sign = "Cancer";
+        }
+               
+        else if (month == 7){
+            if (day < 23)
+            astro_sign = "Cancer";
+            else
+            astro_sign = "Leo";
+        }
+               
+        else if( month == 8){
+            if (day < 23)
+            astro_sign = "Leo";
+            else
+            astro_sign = "Virgo";
+        }
+               
+        else if (month == 9){
+            if (day < 23)
+            astro_sign = "Virgo";
+            else
+            astro_sign = "Libra";
+        }
+               
+        else if (month == 10){
+            if (day < 23)
+            astro_sign = "Libra";
+            else
+            astro_sign = "Scorpio";
+        }
+               
+        else if (month == 11){
+            if (day < 22)
+            astro_sign = "Scorpio";
+            else
+            astro_sign = "Sagittarius";
+		}
+
+			else astro_sign = "invalid";
+	
+    return astro_sign;
+}
+
 export function isValidSign(sign: string) {
 	return sign in examples;
 }
@@ -45,14 +146,10 @@ export async function setHoroscope(
 		signTextId: string,
 		horoscopeTextId: string) {
 
-		
-
 	const signText = document.getElementById(signTextId);
 	const horoscope = document.getElementById(horoscopeTextId);
 	const date = new Date().toLocaleDateString('en-us', {year:"numeric", day:"numeric", month:"short"}) // "Jul 2021 Friday";
   	
-	//`${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()}`;
-
 	if (horoscope) {
 		horoscope.textContent = await buildHoroscope(sign).then((horoscope: string) => {
 			if (signText) {
--- src/Horoscope.tsx
+++ src/Horoscope.tsx
@@ -1,50 +1,70 @@
 import { LoadingButton } from "@mui/lab";
 import { useState } from "react";
 import { TextField } from "@mui/material";
-import './Horoscope.css';
-import { isValidSign, setHoroscope, showDiv } from './Controller';
+import "./Horoscope.css";
+import {
+  isValidSign,
+  findZodiacSign,
+  setHoroscope,
+  showDiv,
+} from "./Controller";
 
 function MyComponent() {
-
   const [loading, setLoading] = useState(false);
   const [userSign, setUserSign] = useState("");
+  const [userBirthDate, setuserBirthDate] = useState<Date|null>(null);
+  //const [userBirthMonth, setuserBirthMonth] = useState(-1);
+  //const [userBirthDay, setuserBirthDay] = useState(-1);
 
   return (
     <>
-      <div style={{paddingTop: "30px", paddingBottom: "30px"}}>
+      <div className=".flex-row">
         <TextField
-            color="secondary"
-            helperText="Enter your name"
-            id="username"
-            label="Name"
-          />
+          color="secondary"
+          //helperText="Enter your name"
+          id="username"
+          label="Name"
+        />
         <TextField
-            color="secondary"
-            helperText="Enter your sign: Aries, Taurus, Gemini, etc."
-            id="sign"
-            label="Sign"
-            onChange={(e: object) => {
-              setUserSign((e as any).target.value);
-            }}
-          />
+          id="date"
+          label="Date & Time of Birth"
+          type="date"
+          defaultValue=""
+          sx={{ width: 250 }}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          onChange={(e:any) => {
+            //console.log(new Date(e.target.value));
+            //setuserBirthMonth(new Date (e.target.value as Date).getMonth());
+            //setuserBirthDay(new Date (e.target.value as Date).getDay());
+            //setuserBirthDate(new Date(e.target.value));
+            //console.log(new Date(e.target.value));
+            console.log(e.target.value);
+          }}
+          
+        />
       </div>
       <LoadingButton
         loading={loading}
         id="mainButton"
         variant="contained"
+        
         onClick={async () => {
-          if (isValidSign(userSign)) {
-            setLoading(true);
-            setHoroscope(userSign, "horoscopeHeader", "horoscopeText").then(() => {
+          if (userBirthDate) {
+            setUserSign(findZodiacSign(userBirthDate));
+          setLoading(true);
+          setHoroscope(userSign, "horoscopeHeader", "horoscopeText").then(
+            () => {
               setLoading(false);
-              showDiv("results")
-            });  
-          }
+              showDiv("results");
+            }
+          );}
         }}
       >
-        Consult 
+        Consult
       </LoadingButton>
-      <div id="results" style={{display:"none"}}>
+      <div id="results" style={{ display: "none" }}>
         <h4 id="horoscopeHeader">Sign here</h4>
         <p id="horoscopeText">Horoscope here</p>
       </div>
