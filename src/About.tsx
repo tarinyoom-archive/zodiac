1885ddda96c03fd611c892172f6bec66b3f5ebc2
ac-reynolds <adam.chiu.reynolds@gmail.com>
2022-12-05 09:01:49 -0800

    adds description

--- src/About.tsx
+++ src/About.tsx
@@ -1,30 +1,22 @@
-import './App.css';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: "#dac9f4",
-      contrastText: '#121212',
-    }
-  }
-});
+import './About.css';
 
 function About() {
   
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
       <div className="App">
-        <header className="App-header">
-          <h1>zo:diac</h1>
-        </header>
-        
+		<a href="/">Back to zo:diac</a>
+		<h4>What is zo:diac?</h4>
+		<p>zo:diac is a horoscope reader powered by a neural network provided by <a href="https://cohere.ai/">co:here</a>.</p>
+		<h4>What's a neural network?</h4>
+		<p>A neural network is an artificial intelligence program that learns in this case by reading text.
+			It's fed data from public sources, and learns the information within. Learn more about co:here's datasets <a href="https://docs.cohere.ai/docs/data-statement">here</a>.</p>
+		<h4>Does it actually know my horoscope?</h4>
+		<p>The neural network can draw astrology information from any source it reads from, including the <a href="https://lib.msu.edu/gds/">Google Books Dataset</a> and <a href="https://tumblr.com">Tumblr</a>, both of which contain significant amounts of astrology content.
+			Neural networks are excellent at picking up and identifying trends, so it can sense patterns in astrology that even the best human astrologers might not be able to pick up on.</p>
+		<footer>
+            Developed by Adam Reynolds and Aditya Tuladhar.
+        </footer>
       </div>
-    </ThemeProvider>
-
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,29 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import About from './About';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-  },
-  {
-    path: "/about",
-    element: <About/>
-  }
-]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-        <RouterProvider router={router} />
+        <App />
   </React.StrictMode>
 );
 
