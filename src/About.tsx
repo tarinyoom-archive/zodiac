429eef5c49470f4a5b4d88d4b246b54f95c5f45c
ac-reynolds <adam.chiu.reynolds@gmail.com>
2022-12-16 12:42:42 -0800

    prunes about page

--- src/About.tsx
+++ src/About.tsx
@@ -8,12 +8,10 @@ function About() {
 		<NavLink to="/">Back to zo:diac</NavLink>
 		<h4>What is zo:diac?</h4>
 		<p>zo:diac is a horoscope reader powered by a neural network provided by <a href="https://cohere.ai/">co:here</a>. It creates a unique horoscope for you based on your information and your topics of interest.</p>
-		<h4>What's a neural network?</h4>
-		<p>A neural network is an artificial intelligence program that learns in this case by reading text.
-			It's fed data from public sources, and learns the information within. Learn more about co:here's datasets <a href="https://docs.cohere.ai/docs/data-statement">here</a>.</p>
 		<h4>Does it actually know my horoscope?</h4>
-		<p>The neural network can draw astrology information from any source it reads from, including the <a href="https://lib.msu.edu/gds/">Google Books Dataset</a> and <a href="https://tumblr.com">Tumblr</a>, both of which contain significant amounts of astrology content.
-			Neural networks are excellent at picking up and identifying trends, so it can sense patterns in astrology that even the best human astrologers might not be able to pick up on.</p>
+		<p>This neural network learns in the same ways humans do-- by reading! It draws astrology information from publicly available blogs and books,
+			and can pick up patterns and form opinions based on what it's seen.
+		</p>
 		<footer>
             Developed by Adam Reynolds and Aditya Tuladhar.
         </footer>
